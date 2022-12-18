434868777f4b1a4551b8a82236a349f2c4b4bb4f
DANILO <danilofoxlog@gmail.com>
2022-12-18 10:32:23 -0300

    Implementa class domain e Enum expenseType

---
+++ src/main/java/br/com/danilosilva/despesa/despesa/ExpenseType.java
@@ -0,0 +1,18 @@
+package br.com.danilosilva.despesa.despesa;
+
+public enum ExpenseType {
+    FOOD,
+    SUBSCRIPTION,
+    PERSONAL_CARE,
+    SHOPPING,
+    DEBT,
+    LOAN,
+    EDUCATION,
+    INVESTMENT,
+    LEISURE,
+    MARKET,
+    HEALTH,
+    WORK,
+    TRANSPORTATION,
+    TRAVEL;
+}
