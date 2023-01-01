9e32467ddd54e158dc2ba482ee99e31e4bb1415c
Danilo1dev <danilofoxlog@gmail.com>
2023-01-01 17:14:34 -0300

    Teste da class principal

--- src/test/java/br/com/danilosilva/despesa/DespesaApplicationTests.java
+++ src/test/java/br/com/danilosilva/despesa/DespesaApplicationTests.java
@@ -1,13 +1,19 @@
 package br.com.danilosilva.despesa;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Log4j2
 class DespesaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void getHomeTest() {
+	}
+	@Test
+	void main() {
+		DespesaApplication.main(new String[] {});
 	}
 
 }
