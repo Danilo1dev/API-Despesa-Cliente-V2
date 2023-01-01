d85add97bf231a518e8196944b39893c930eb9fb
Danilo1dev <danilofoxlog@gmail.com>
2023-01-01 18:37:44 -0300

    Cria class MockPeople para teste

---
+++ src/test/java/br/com/danilosilva/despesa/pessoa/application/mock/MockPeople.java
@@ -0,0 +1,25 @@
+package br.com.danilosilva.despesa.pessoa.application.mock;
+
+import br.com.danilosilva.despesa.pessoa.domain.People;
+import br.com.danilosilva.despesa.pessoa.domain.Sex;
+import lombok.Builder;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+public class MockPeople {
+
+    public static People peopleBuild() {
+        return People.builder()
+                .idPeople(UUID.randomUUID())
+                .namePeople("Danilo Silva")
+                .cpf("12345678901")
+                .age("30")
+                .sex(Sex.valueOf("MASCUINO"))
+                .email("danilo@email.com")
+                .income(new BigDecimal("1000.00"))
+                .dateTimeRegistration(LocalDateTime.now())
+                .build();
+    }
+}
