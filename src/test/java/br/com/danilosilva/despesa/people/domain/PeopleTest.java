75746e742401ae7c2cd07261b4fa04c16e009067
Danilo1dev <danilofoxlog@gmail.com>
2023-01-11 20:56:49 -0300

    Implementa testPeople do domain

---
+++ src/test/java/br/com/danilosilva/despesa/people/domain/PeopleTest.java
@@ -0,0 +1,68 @@
+package br.com.danilosilva.despesa.people.domain;
+
+import br.com.danilosilva.despesa.people.application.mock.MockPeople;
+import br.com.danilosilva.despesa.people.application.repository.PeopleRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PeopleTest {
+    @InjectMocks
+    private People people;
+    @Mock
+    private PeopleRepository peopleRepository;
+    @Test
+    void getNamePeople() {
+        when(peopleRepository.searchPersonById(any())).thenReturn(MockPeople.peopleBuild());
+        People people = peopleRepository.searchPersonById(any());
+        assertNotNull(people);
+    }
+    @Test
+    void getCpf() {
+        when(peopleRepository.searchPersonById(any())).thenReturn(MockPeople.peopleBuild());
+        People people = peopleRepository.searchPersonById(any());
+        assertNotNull(people);
+        assertNotNull(people.getCpf());
+    }
+    @Test
+    void getAge() {
+        when(peopleRepository.searchPersonById(any())).thenReturn(MockPeople.peopleBuild());
+        People people = peopleRepository.searchPersonById(any());
+        assertNotNull(people);
+        assertNotNull(people.getAge());
+    }
+    @Test
+    void getSex() {
+        when(peopleRepository.searchPersonById(any())).thenReturn(MockPeople.peopleBuild());
+        People people = peopleRepository.searchPersonById(any());
+        assertNotNull(people);
+        assertNotNull(people.getSex());
+    }
+    @Test
+    void getTelephone() {
+        when(peopleRepository.searchPersonById(any())).thenReturn(MockPeople.peopleBuild());
+        People people = peopleRepository.searchPersonById(any());
+        assertNotNull(people);
+        assertNotNull(people.getTelephone());
+    }
+    @Test
+    void getEmail() {
+        when(peopleRepository.searchPersonById(any())).thenReturn(MockPeople.peopleBuild());
+        People people = peopleRepository.searchPersonById(any());
+        assertNotNull(people);
+        assertNotNull(people.getEmail());
+    }
+    @Test
+    void getIncome() {
+        when(peopleRepository.searchPersonById(any())).thenReturn(MockPeople.peopleBuild());
+        People people = peopleRepository.searchPersonById(any());
+        assertNotNull(people);
+        assertNotNull(people.getIncome());
+    }
+}
