93c2599b4a6fb484b0029bd727f6ff4ddac2d0bd
Danilo1dev <danilofoxlog@gmail.com>
2023-01-15 11:17:14 -0300

    Implementa test create, everyBody, personViaID na class PeopleControllerTest

--- src/test/java/br/com/danilosilva/despesa/people/application/api/PeopleControllerTest.java
+++ src/test/java/br/com/danilosilva/despesa/people/application/api/PeopleControllerTest.java
@@ -1,13 +1,17 @@
 package br.com.danilosilva.despesa.people.application.api;
 
 import br.com.danilosilva.despesa.people.application.service.PeopleService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PeopleControllerTest {
@@ -19,20 +23,33 @@ class PeopleControllerTest {
     private PeopleRequest peopleRequest;
     @Mock
     private PeopleResponse peopleResponse;
+    @Mock
+    private PeopleListResponse peopleListResponse;
+    @Mock
+    private PeopleDetailedResponse peopleDetailedResponse;
     @Test
     void peopleCreate() {
         when(peopleService.createPeople(peopleRequest)).thenReturn(peopleResponse);
         peopleController.postPeople(peopleRequest);
         verify(peopleService).createPeople(peopleRequest);
+        verify(peopleService,times (1)).createPeople(any());
     }
     @Test
     void getEveryBody() {
+        when(peopleService.searchAllPeople()).thenReturn(List.of(peopleListResponse));
+        List<PeopleListResponse> peopleListResponses = peopleController.getEveryBody();
+        verify(peopleService).searchAllPeople();
+        verify(peopleService,times (1)).searchAllPeople();
+        assertEquals(1,peopleListResponses.size());
     }
-
     @Test
     void getPersonViaID() {
+        when(peopleService.getPersonViaID(any())).thenReturn(peopleDetailedResponse);
+        PeopleDetailedResponse peopleDetailedResponse = peopleController.getPersonViaID(any());
+        verify(peopleService).getPersonViaID(any());
+        verify(peopleService,times (1)).getPersonViaID(any());
+        Assertions.assertNotNull(peopleDetailedResponse);
     }
-
     @Test
     void deletePersonViaID() {
     }
