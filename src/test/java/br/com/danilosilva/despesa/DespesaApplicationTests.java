000835ec81c76030ab933e8348ad5c2c0c1f310f
Danilo1dev <danilofoxlog@gmail.com>
2023-01-15 12:12:31 -0300

    Refactor no Test getHomeTest na class DespesaApplicationTests

--- src/test/java/br/com/danilosilva/despesa/DespesaApplicationTests.java
+++ src/test/java/br/com/danilosilva/despesa/DespesaApplicationTests.java
@@ -2,18 +2,24 @@ package br.com.danilosilva.despesa;
 
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Log4j2
+@ExtendWith(MockitoExtension.class)
 class DespesaApplicationTests {
+	@InjectMocks
+	private DespesaApplication despesaApplication;
 
 	@Test
 	void getHomeTest() {
+		despesaApplication.getHomeTest();
 	}
 	@Test
 	void main() {
 		DespesaApplication.main(new String[] {});
 	}
-
 }
